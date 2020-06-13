1b4a09cc3be72f1c08b45333f4cf13ae33650384
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-06-13 13:15:20 -0300

    Create repository

---
+++ src/repositories/TransactionsRepository.ts
@@ -0,0 +1,45 @@
+import { EntityRepository, Repository } from 'typeorm';
+
+import Transaction from '../models/Transaction';
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+@EntityRepository(Transaction)
+class TransactionsRepository extends Repository<Transaction> {
+  public async getBalance(): Promise<Balance> {
+    const transactions = await this.find();
+
+    const balance = transactions.reduce(
+      (accumulator: Balance, { type, value }: Transaction) => {
+        switch (type) {
+          case 'income':
+            accumulator.income += value;
+            accumulator.total += value;
+            break;
+
+          case 'outcome':
+            accumulator.outcome += value;
+            accumulator.total -= value;
+            break;
+
+          default:
+        }
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    return balance;
+  }
+}
+
+export default TransactionsRepository;
