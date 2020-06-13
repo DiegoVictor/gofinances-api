81a17296e0a6c73f8091db1093b9c6e13e876477
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-06-13 13:19:57 -0300

    Return transactions

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 import multer from 'multer';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 import DeleteTransactionService from '../services/DeleteTransactionService';
 import ImportTransactionsService from '../services/ImportTransactionsService';
@@ -8,6 +11,16 @@ import upload_configuration from '../config/upload';
 
 const transactionsRouter = Router();
 const upload = multer(upload_configuration);
+
+transactionsRouter.get('/', async (_, response) => {
+  const transactions_repository = getCustomRepository(TransactionsRepository);
+
+  const transactions = await transactions_repository.find();
+  const balance = await transactions_repository.getBalance();
+
+  return response.json({ transactions, balance });
+});
+
 transactionsRouter.post('/', async (request, response) => {
   const { title, value, type, category } = request.body;
 
