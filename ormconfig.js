f10763a45aea1e3349d05d451ab90373dd8dcf71
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-06-14 14:46:48 -0300

    Read values from env file

---
+++ ormconfig.js
@@ -0,0 +1,13 @@
+module.exports = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  entities: ['./src/models/*.ts'],
+  migrations: ['./src/database/migrations/*.ts'],
+  cli: {
+    migrationsDir: './src/database/migrations',
+  },
+};
