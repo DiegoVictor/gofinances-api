65ae2b1c45d4e188b544ae8cc97a35498d24dc6f
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-01 21:08:27 -0300

    Set database name

--- ormconfig.js
+++ ormconfig.js
@@ -5,7 +5,7 @@ module.exports = {
   port: process.env.POSTGRES_PORT,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
+  database: `${process.env.POSTGRES_DATABASE}-${process.env.NODE_ENV}`,
   entities: ['./src/models/*.ts'],
   migrations: ['./src/database/migrations/*.ts'],
   cli: {
