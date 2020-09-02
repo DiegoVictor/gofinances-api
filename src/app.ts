c127f1d3908785a79e148f894036d4825b26f36d
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-01 21:09:35 -0300

    Install celebrate

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import helmet from 'helmet';
+import { errors } from 'celebrate';
 
 import routes from './routes';
 import AppError from './errors/AppError';
@@ -19,6 +20,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
+app.use(errors());
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
