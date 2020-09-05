aad4288f77c9063b95e9e427932095578ce2852c
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-05 12:42:44 -0300

    Configure rate limit for all routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 
 import transactionsRouter from './transactions.routes';
+import RateLimit from '../middlewares/RateLimit';
 
 const routes = Router();
+
+routes.use(RateLimit);
 
 routes.use('/transactions', transactionsRouter);
 
