7da0de8c39abd0b77c0627d13a38b9980479888c
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-06 01:09:59 -0300

    Create transaction controller

---
+++ src/controllers/TransactionsController.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
+
+export default class TransactionsController {
+  async index(_: Request, response: Response): Promise<Response> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transactions = await transactionsRepository.find();
+
+    const count = await transactionsRepository.count();
+    response.header('X-Total-Count', count.toString());
+
+    const balance = await transactionsRepository.getBalance();
+
+    return response.json({ transactions, balance });
+  }
+
+  async store(request: Request, response: Response): Promise<Response> {
+    const { title, value, type, category } = request.body;
+
+    const createTransaction = new CreateTransactionService();
+    const transaction = await createTransaction.execute({
+      title,
+      value,
+      type,
+      category,
+    });
+
+    return response.json(transaction);
+  }
+
+  async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const deleteTransaction = new DeleteTransactionService();
+    await deleteTransaction.execute({ id });
+
+    return response.sendStatus(204);
+  }
+}
