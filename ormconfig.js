289244cad15cb78c0c0dd9407fc55806279f671b
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-07 12:52:01 -0300

    Control database used

--- ormconfig.js
+++ ormconfig.js
@@ -5,7 +5,10 @@ module.exports = {
   port: process.env.POSTGRES_PORT,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-  database: `${process.env.POSTGRES_DATABASE}-${process.env.NODE_ENV}`,
+  database:
+    process.env.NODE_ENV !== 'test'
+      ? 'tests'
+      : `${process.env.POSTGRES_DATABASE}`,
   entities: ['./src/models/*.ts'],
   migrations: ['./src/database/migrations/*.ts'],
   cli: {
