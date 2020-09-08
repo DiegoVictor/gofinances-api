b7e837e096ede1bad48ad787acd4d73b7ccd8a16
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-08 19:15:48 -0300

    Update and move to folder

--- tests/integration/Transaction.spec.ts
+++ tests/integration/Transaction.spec.ts
@@ -1,16 +1,28 @@
 import request from 'supertest';
 import path from 'path';
 import { Connection, getRepository, getConnection } from 'typeorm';
-import createConnection from '../src/database';
 
-import Transaction from '../src/models/Transaction';
-import Category from '../src/models/Category';
+import app from '../../src/app';
+import createConnection from '../../src/database/typeorm';
+import Transaction from '../../src/models/Transaction';
+import Category from '../../src/models/Category';
+import factory from '../utils/factory';
 
-import app from '../src/app';
+interface TransactionItem {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
-let connection: Connection;
+jest.mock('../../src/config/security', () => ({
+  duration: 1,
+  points: 20,
+}));
 
 describe('Transaction', () => {
+  let connection: Connection;
+
   beforeAll(async () => {
     connection = await createConnection('test-connection');
 
@@ -34,28 +46,27 @@ describe('Transaction', () => {
   });
 
   it('should be able to list transactions', async () => {
-    await request(app).post('/transactions').send({
-      title: 'March Salary',
-      type: 'income',
-      value: 4000,
-      category: 'Salary',
-    });
+    const transactions = await factory.attrsMany<TransactionItem>(
+      'Transaction',
+      3,
+      [
+        { type: 'income', value: 4000 },
+        { type: 'income', value: 4000 },
+        { type: 'outcome', value: 6000 },
+      ],
+    );
 
-    await request(app).post('/transactions').send({
-      title: 'April Salary',
-      type: 'income',
-      value: 4000,
-      category: 'Salary',
-    });
+    async function saveTransaction(array: TransactionItem[]): Promise<void> {
+      const item = array.shift();
+      await request(app).post('/v1/transactions').send(item);
 
-    await request(app).post('/transactions').send({
-      title: 'Macbook',
-      type: 'outcome',
-      value: 6000,
-      category: 'Eletronics',
-    });
+      if (array.length > 0) {
+        await saveTransaction(array);
+      }
+    }
+    await saveTransaction(transactions);
 
-    const response = await request(app).get('/transactions');
+    const response = await request(app).get('/v1/transactions');
 
     expect(response.body.transactions).toHaveLength(3);
     expect(response.body.balance).toMatchObject({
@@ -67,167 +78,181 @@ describe('Transaction', () => {
 
   it('should be able to create new transaction', async () => {
     const transactionsRepository = getRepository(Transaction);
+    const categoriesRepository = getRepository(Category);
+    const { title, type, value, category: categoryTitle } = await factory.attrs<
+      TransactionItem
+    >('Transaction', { type: 'income' });
 
-    const response = await request(app).post('/transactions').send({
-      title: 'March Salary',
-      type: 'income',
-      value: 4000,
-      category: 'Salary',
-    });
+    const response = await request(app)
+      .post('/v1/transactions')
+      .send({ title, type, value, category: categoryTitle });
 
     const transaction = await transactionsRepository.findOne({
-      where: {
-        title: 'March Salary',
-      },
+      where: { title },
     });
 
     expect(transaction).toBeTruthy();
 
-    expect(response.body).toMatchObject(
-      expect.objectContaining({
-        id: expect.any(String),
-      }),
-    );
-  });
-
-  it('should not be able to create new transaction with wrong type', async () => {
-    const response = await request(app).post('/transactions').send({
-      title: 'March Salary',
-      type: '',
-      value: 4000,
-      category: 'Salary',
+    const category = await categoriesRepository.findOne({
+      where: { title: categoryTitle },
     });
 
-    expect(response.body).toStrictEqual({
-      message: 'Invalid transaction type',
-      status: 'error',
+    expect(category).toBeTruthy();
+
+    expect(response.body).toMatchObject({
+      id: transaction?.id,
+      category_id: category?.id,
+      title,
+      type,
+      value,
     });
   });
 
   it('should create tags when inserting new transactions', async () => {
     const transactionsRepository = getRepository(Transaction);
     const categoriesRepository = getRepository(Category);
+    const { title, type, value, category: categoryTitle } = await factory.attrs<
+      TransactionItem
+    >('Transaction', { type: 'income' });
 
-    const response = await request(app).post('/transactions').send({
-      title: 'March Salary',
-      type: 'income',
-      value: 4000,
-      category: 'Salary',
+    const response = await request(app).post('/v1/transactions').send({
+      title,
+      type,
+      value,
+      category: categoryTitle,
     });
 
     const category = await categoriesRepository.findOne({
-      where: {
-        title: 'Salary',
-      },
+      where: { title: categoryTitle },
     });
 
     expect(category).toBeTruthy();
 
     const transaction = await transactionsRepository.findOne({
       where: {
-        title: 'March Salary',
+        title,
         category_id: category?.id,
       },
     });
 
     expect(transaction).toBeTruthy();
 
-    expect(response.body).toMatchObject(
-      expect.objectContaining({
-        id: expect.any(String),
-      }),
-    );
+    expect(response.body).toMatchObject({
+      id: transaction?.id,
+      category_id: category?.id,
+      title,
+      type,
+      value,
+    });
   });
 
   it('should not create tags when they already exists', async () => {
     const transactionsRepository = getRepository(Transaction);
     const categoriesRepository = getRepository(Category);
+    const { title, type, value, category } = await factory.attrs<
+      TransactionItem
+    >('Transaction', { type: 'income' });
 
     const { identifiers } = await categoriesRepository.insert({
-      title: 'Salary',
+      title: category,
     });
 
-    const insertedCategoryId = identifiers[0].id;
+    const [{ id: category_id }] = identifiers;
 
-    await request(app).post('/transactions').send({
-      title: 'March Salary',
-      type: 'income',
-      value: 4000,
-      category: 'Salary',
+    await request(app).post('/v1/transactions').send({
+      title,
+      type,
+      value,
+      category,
     });
 
     const transaction = await transactionsRepository.findOne({
       where: {
-        title: 'March Salary',
-        category_id: insertedCategoryId,
+        title,
+        category_id,
       },
     });
+    expect(transaction).toBeTruthy();
 
     const categoriesCount = await categoriesRepository.find();
 
     expect(categoriesCount).toHaveLength(1);
-    expect(transaction).toBeTruthy();
   });
 
   it('should not be able to create outcome transaction without a valid balance', async () => {
-    await request(app).post('/transactions').send({
-      title: 'March Salary',
-      type: 'income',
-      value: 4000,
-      category: 'Salary',
-    });
+    const [
+      { title, type, value, category },
+      transaction,
+    ] = await factory.attrsMany<TransactionItem>('Transaction', 2, [
+      { type: 'income' },
+      { type: 'outcome' },
+    ]);
 
-    const response = await request(app).post('/transactions').send({
-      title: 'iPhone',
-      type: 'outcome',
-      value: 4500,
-      category: 'Eletronics',
-    });
+    await request(app)
+      .post('/v1/transactions')
+      .send({ title, type, value, category });
 
-    expect(response.status).toBe(400);
-    expect(response.body).toMatchObject(
-      expect.objectContaining({
-        status: 'error',
-        message: expect.any(String),
-      }),
-    );
+    const response = await request(app)
+      .post('/v1/transactions')
+      .expect(401)
+      .send({
+        ...transaction,
+        value: value + 500,
+      });
+
+    expect(response.body).toMatchObject({
+      status: 'error',
+      message: "You don't have enough money to this transaction!",
+      code: 141,
+      docs: process.env.DOCS_URL,
+    });
   });
 
   it('should be able to delete a transaction', async () => {
     const transactionsRepository = getRepository(Transaction);
-
-    const response = await request(app).post('/transactions').send({
-      title: 'March Salary',
+    const transaction = await factory.attrs<TransactionItem>('Transaction', {
       type: 'income',
-      value: 4000,
-      category: 'Salary',
     });
 
-    await request(app).delete(`/transactions/${response.body.id}`);
+    const response = await request(app)
+      .post('/v1/transactions')
+      .send(transaction);
 
-    const transaction = await transactionsRepository.findOne(response.body.id);
+    await request(app).delete(`/v1/transactions/${response.body.id}`);
 
-    expect(transaction).toBeFalsy();
+    const deletedTransaction = await transactionsRepository.findOne(
+      response.body.id,
+    );
+
+    expect(deletedTransaction).toBeFalsy();
   });
 
   it('should not be able to delete a transaction that not exists', async () => {
     const transactionsRepository = getRepository(Transaction);
+    const { title, type, value } = await factory.attrs<TransactionItem>(
+      'Transaction',
+      {
+        type: 'income',
+      },
+    );
 
     const transaction = await transactionsRepository.create({
-      title: 'March Salary',
-      type: 'income',
-      value: 4000,
+      title,
+      type,
+      value,
     });
     await transactionsRepository.save(transaction);
     await transactionsRepository.delete(transaction.id);
 
     const response = await request(app)
-      .delete(`/transactions/${transaction.id}`)
+      .delete(`/v1/transactions/${transaction.id}`)
       .expect(404);
 
     expect(response.body).toStrictEqual({
       status: 'error',
       message: 'Transaction not found',
+      code: 144,
+      docs: process.env.DOCS_URL,
     });
   });
 
@@ -235,9 +260,16 @@ describe('Transaction', () => {
     const transactionsRepository = getRepository(Transaction);
     const categoriesRepository = getRepository(Category);
 
-    const importCSV = path.resolve(__dirname, 'import_template.csv');
+    const importCSV = path.resolve(
+      __dirname,
+      '..',
+      'files',
+      'import_template.csv',
+    );
 
-    await request(app).post('/transactions/import').attach('file', importCSV);
+    await request(app)
+      .post('/v1/transactions/import')
+      .attach('file', importCSV);
 
     const transactions = await transactionsRepository.find();
     const categories = await categoriesRepository.find();
@@ -277,14 +309,21 @@ describe('Transaction', () => {
     const transactionsRepository = getRepository(Transaction);
     const categoriesRepository = getRepository(Category);
 
-    const importCSV = path.resolve(__dirname, 'import_template.csv');
+    const importCSV = path.resolve(
+      __dirname,
+      '..',
+      'files',
+      'import_template.csv',
+    );
 
     const category = categoriesRepository.create({
       title: 'Food',
     });
     await categoriesRepository.save(category);
 
-    await request(app).post('/transactions/import').attach('file', importCSV);
+    await request(app)
+      .post('/v1/transactions/import')
+      .attach('file', importCSV);
 
     const transactions = await transactionsRepository.find();
     const categories = await categoriesRepository.find();
