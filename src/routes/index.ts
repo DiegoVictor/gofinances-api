9b991a8d2b6509973a78161bfd47ca406c7d7201
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-10-22 23:28:51 -0300

    Remove rate limit and redis

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,8 @@
 import { Router } from 'express';
 
 import transactionsRouter from './transactions.routes';
-import RateLimit from '../middlewares/RateLimit';
 
 const routes = Router();
-
-routes.use(RateLimit);
 
 routes.use('/transactions', transactionsRouter);
 
